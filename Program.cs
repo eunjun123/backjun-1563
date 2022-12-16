321df0518c34835a7e4e36de67f9c77070af7d58
eunjun123 <113625608+eunjun123@users.noreply.github.com>
2022-12-16 19:39:53 +0900

    초기 커밋.

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread myThread = new Thread(Func);
+            ThreadParam tp = new ThreadParam(2, 6);
+            myThread.Start(tp);
+            for (int i = 0; i < 3; i++)
+            {
+                Console.WriteLine(i + 1);
+                Thread.Sleep(100);
+            }
+            Console.WriteLine("메인스레드 종료");
+        }
+        private static void Func(object obj)
+        {
+            ThreadParam tempParam = obj as ThreadParam;
+            int num = tempParam.param1 + tempParam.param2;
+            for (int i = 0; i < num; i++)
+            {
+                Console.WriteLine(i + 1);
+                Thread.Sleep(100);
+            }
+        }
+    }
+    class ThreadParam
+    {
+        public int param1;
+        public int param2;
+        public ThreadParam(int num1, int num2)
+        {
+            this.param1 = num1; this.param2 = num2;
+        }
+    }
